1b05cc30a78dba2bb80790a1c368ac51b8817f6c
Abdofaredov <abdoofaredd@gmail.com>
2024-02-18 05:37:01 +0200

    counterApp_with_bloc

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:blocapps/cubit/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterInitialState());
+
+  int teamAPoints = 0;
+
+  int teamBPoints = 0;
+
+  void teamIncrement({required String team, required int buttonNumber}) {
+    if (team == 'A') {
+      teamAPoints += buttonNumber;
+      emit(CounterAIncrementState());
+    } else {
+      teamBPoints += buttonNumber;
+      emit(CounterBIncrementState());
+    }
+  }
+
+  void resetNumber() {
+    teamAPoints = 0;
+    teamBPoints = 0;
+    emit(CounterResetState());
+  }
+}
